e0506f72d89d1d83155e011947cf6ac3ff88c51a
André Neto-Bradley <66263560+anetobradley@users.noreply.github.com>
2021-08-13 16:35:56 +0100

    Update EPC_Search_Fn.R

--- 02-EnergyModel/EPC_Search_Fn.R
+++ 02-EnergyModel/EPC_Search_Fn.R
@@ -8,7 +8,7 @@ postcode <- "CB3"
 
 epc_root <- paste0("https://epc.opendatacommunities.org/api/v1/domestic/search?accept=application/json&postcode=",postcode)
 
-epc_pull <- httr::GET(url=epc_root, authenticate("apn30@cam.ac.uk","d162f191c46b4aae29e4adfbfa5a30b63891f556"), accept_json())
+epc_pull <- httr::GET(url=epc_root, authenticate("apn30@cam.ac.uk","INSERT KEY"), accept_json())
 httr::http_status(epc_pull)
 
 epc_data <-  jsonlite::fromJSON(txt=httr::content(epc_pull, "text"))
